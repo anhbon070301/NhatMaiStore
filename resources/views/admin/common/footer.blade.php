3355a33470da2dca639dee927dc875cccc5bf611
you <anhemtn114@gmail.com>
2023-07-24 15:56:52 +0700

    update

--- resources/views/admin/common/footer.blade.php
+++ resources/views/admin/common/footer.blade.php
@@ -13,10 +13,10 @@
   </footer><!-- End Footer -->
 
   <!-- Vendor JS Files -->
-  <script src="{{ asset('css/assets/vendor/apexcharts/apexcharts.min.js') }}"></script>
-  <script src="{{ asset('css/assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-  <script src="{{ asset('css/assets/vendor/chart.js/chart.umd.js') }}"></script>
-  <script src="{{ asset('css/assets/vendor/echarts/echarts.min.js') }}"></script>
-  <script src="{{ asset('css/assets/vendor/quill/quill.min.js') }}"></script>
-  <script src="{{ asset('css/assets/vendor/simple-datatables/simple-datatables.js') }}"></script>
-  <script src="{{ asset('css/assets/vendor/php-email-form/validate.js') }}"></script>
+  <script src="{{ asset('assets/vendor/apexcharts/apexcharts.min.js') }}"></script>
+  <script src="{{ asset('assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+  <script src="{{ asset('assets/vendor/chart.js/chart.umd.js') }}"></script>
+  <script src="{{ asset('assets/vendor/echarts/echarts.min.js') }}"></script>
+  <script src="{{ asset('assets/vendor/quill/quill.min.js') }}"></script>
+  <script src="{{ asset('assets/vendor/simple-datatables/simple-datatables.js') }}"></script>
+  <script src="{{ asset('assets/vendor/php-email-form/validate.js') }}"></script>
